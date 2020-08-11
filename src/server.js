b974423fb84361d4853a71a97cd55a4103e7de79
natansouzaa <natan.souza@ccc.ufcg.edu.br>
2020-08-10 23:29:38 -0300

    Backend da aplicacao finalizado

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+//Dados
 const proffys = [
     { 
         name: "Diego Fernandes",
@@ -23,28 +24,74 @@ const proffys = [
     }
 ]
 
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+//Funcionalidades
+
+function getSubject(subjectNumber) {
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
+
 function pageLanding(req, res) {
     return res.render("index.html")
 }
 
 function pageStudy(req, res) {
-    return res.render("study.html", { proffys })
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
 
 function pageGiveClasses(req, res) {
-    return res.render("give-classes.html")
+    //Adicionar dados a lista de proffys
+    const data = req.query
+    const isNotEmpty = Object.keys(data).length > 0
+
+    if (isNotEmpty) {
+
+        data.subject = getSubject(data.subject)
+
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }
+
+    return res.render("give-classes.html", { subjects, weekdays })
 }
 
+//Servidor
 const express = require("express")
 const server = express()
 
-//Configurar nunjucks
+//Configurar nunjucks (template engine)
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
     express: server,
     noCache: true
 })
 
+//Inicio e configuração do servidor
 server
 //Configurar arquivos estáticos (css, scripts, imagens)
 .use(express.static("public"))
@@ -52,4 +99,5 @@ server
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+//Start do servidor
 .listen(5500)
